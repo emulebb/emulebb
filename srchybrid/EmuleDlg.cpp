1513358026e961e0a1e364062203e574b99794e3
Threepwood-7 <mt0z1@pir0z.yxz>
2026-05-08 09:16:14 +0200

    BUG-084 close web admin token handle

--- srchybrid/EmuleDlg.cpp
+++ srchybrid/EmuleDlg.cpp
@@ -3953,15 +3953,27 @@ LRESULT CemuleDlg::OnWebGUIInteraction(WPARAM wParam, LPARAM lParam)
 	case WEBGUIIA_WINFUNC:
 		if (thePrefs.GetWebAdminAllowedHiLevFunc()) {
 			try {
-				HANDLE hToken;
+				struct SScopedTokenHandle
+				{
+					HANDLE hToken;
+					SScopedTokenHandle()
+						: hToken(NULL)
+					{
+					}
+					~SScopedTokenHandle()
+					{
+						if (hToken != NULL)
+							VERIFY(::CloseHandle(hToken));
+					}
+				} token;
 				TOKEN_PRIVILEGES tkp;	// Get a token for this process.
 
-				if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
+				if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &token.hToken))
 					throw 0; //parameterless throw not allowed here
 				LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &tkp.Privileges[0].Luid);
 				tkp.PrivilegeCount = 1;  // one privilege to set
 				tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;	// Get the shutdown privilege for this process.
-				AdjustTokenPrivileges(hToken, FALSE, &tkp, 0, (PTOKEN_PRIVILEGES)NULL, 0);
+				AdjustTokenPrivileges(token.hToken, FALSE, &tkp, 0, (PTOKEN_PRIVILEGES)NULL, 0);
 
 				if (lParam == 1) // shutdown
 					ExitWindowsEx(EWX_SHUTDOWN | EWX_FORCE, 0);
