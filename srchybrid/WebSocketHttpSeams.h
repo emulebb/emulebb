c5a2794c15e1570e6d6131d92da6ad524098b46a
Threepwood-7 <mt0z1@pir0z.yxz>
2026-05-08 20:24:04 +0200

    CI-019 raise WebServer stress thread reserve

--- srchybrid/WebSocketHttpSeams.h
+++ srchybrid/WebSocketHttpSeams.h
@@ -15,7 +15,7 @@ namespace WebSocketHttpSeams
 static const uint64_t kMaxHttpContentLength = 16ui64 * 1024ui64 * 1024ui64;
 static const uint64_t kMaxHttpHeaderLength = 64ui64 * 1024ui64;
 static const uint32_t kAcceptedClientIoTimeoutMs = 30000u;
-static const size_t kMaxAcceptedClientThreads = 64u;
+static const size_t kMaxAcceptedClientThreads = 128u;
 
 enum class EHttpHeaderScanResult
 {
